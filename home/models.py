f6d90b522a85af6e1a54c727fc7ca99dd19626b5
Diego <dirusali@gmail.com>
2020-03-09 09:48:23 +0100

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
+# I create a model with the required fields, I make them all mandatory to fill and to validate the iban I enforce
+# a lenth of 24 characters for that field
 
-# I create a model the required fields and validation
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -7,22 +8,16 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Profile(models.Model):
-    firstname = models.CharField(max_length=500, blank=True)
-    lastname = models.CharField(max_length=500, blank=True)
-    iban = models.CharField(max_length=500, blank=True)
-
-
+    firstname = models.CharField(max_length=500, blank=False, null=False)
+    lastname = models.CharField(max_length=500, blank=False,null=False)
+    iban = models.CharField(max_length=500, blank=False, null=False)    
     
     def save(self, *args, **kwargs):
        try:
-           if len(self.firstname) > 0 and len(self.lastname) > 0:
-               if len(self.iban) == 24:
-                   super(Profile, self).save(*args, **kwargs)
-               else:
-                   print('please enter a valid 24 digits iban code')    
-           else:
-               print('please enter valid name and surname')
+           if len(self.iban) == 24:
+                super(Profile, self).save(*args, **kwargs)
        except Excpetion as e:
+           print('please enter a valid 24 digits iban code')    
            print(e)
 
 
